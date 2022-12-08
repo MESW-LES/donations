41522a9b0104ce28d593ff8fa98c55a021707d51
DIR\tamires.viegas <trviegas@ext.worten.pt>
2022-12-08 22:54:19 +0000

    register-company

--- frontend/donations-spa/pages/register-company.tsx
+++ frontend/donations-spa/pages/register-company.tsx
@@ -1,10 +1,24 @@
+import { Dropdown } from 'primereact/dropdown';
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const RegisterCompany = () => {
+  const [taxnumber, setTaxnumber] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const categories = [
+    { label: 'Cateogry example 1', value: '1' },
+    { label: 'Cateogry example 1', value: '2' }
+  ];
+
   return (
     <>
       <div className="flex justify-center pt-10">
@@ -20,10 +34,73 @@ const RegisterCompany = () => {
             <div className="w-2"></div>
             <div className="w-8">
               <div className="grid grid-cols-2">
+                <p className="w-4 text-black text-xl">Company Number</p>
+                <InputText
+                  required
+                  className="w-8 bg-white"
+                  style={{ color: "black" }}
+                  value={taxnumber}
+                  onChange={({ target }) => setTaxnumber(target?.value)}
+                />
+              </div>
+            </div>
+            <div className="w-2"></div>
+          </div>
+          <div className="grid grid-cols-3 pt-10">
+            <div className="w-2"></div>
+            <div className="w-8">
+              <div className="grid grid-cols-2">
+                <p className="w-4 text-black text-xl">Name Company</p>
+                <InputText
+                  className="w-8 bg-white"
+                  style={{ color: "black" }}
+                  value={name}
+                  onChange={({ target }) => setName(target?.value)}
+                />
+              </div>
+            </div>
+            <div className="w-2"></div>
+          </div>
+          <div className="grid grid-cols-3 pt-10">
+            <div className="w-2"></div>
+            <div className="w-8">
+              <div className="grid grid-cols-2">
+                <p className="w-4 text-black text-xl">Description</p>
+                <InputText
+                  className="w-8 bg-white"
+                  style={{ color: "black" }}
+                  value={description}
+                  onChange={({ target }) => setDescription(target?.value)}
+                />
+              </div>
+            </div>
+            <div className="w-2"></div>
+          </div>
+          <div className="grid grid-cols-3 pt-10">
+            <div className="w-2"></div>
+            <div className="w-8">
+              <div className="grid grid-cols-2">
+                <p className="w-4 text-black text-xl">Phone</p>
+                <InputText
+                  className="w-8 bg-white"
+                  style={{ color: "black" }}
+                  value={phone}
+                  onChange={({ target }) => setPhone(target?.value)}
+                />
+              </div>
+            </div>
+            <div className="w-2"></div>
+          </div>
+          <div className="grid grid-cols-3 pt-10">
+            <div className="w-2"></div>
+            <div className="w-8">
+              <div className="grid grid-cols-2">
                 <p className="w-4 text-black text-xl">Email</p>
                 <InputText
                   className="w-8 bg-white"
                   style={{ color: "black" }}
+                  value={email}
+                  onChange={({ target }) => setEmail(target?.value)}
                 />
               </div>
             </div>
@@ -38,7 +115,19 @@ const RegisterCompany = () => {
                   type={"password"}
                   className="w-8 bg-white"
                   style={{ color: "black" }}
+                  value={password}
+                  onChange={({ target }) => setPassword(target?.value)}
                 />
+              </div>
+            </div>
+            <div className="w-2"></div>
+          </div>
+          <div className="grid grid-cols-3 pt-10">
+            <div className="w-2"></div>
+            <div className="w-8">
+              <div className="grid grid-cols-2">
+                <p className="w-4 text-black text-xl">Category</p>
+                <Dropdown options={categories} optionLabel="label" placeholder="Categories" />
               </div>
             </div>
             <div className="w-2"></div>
