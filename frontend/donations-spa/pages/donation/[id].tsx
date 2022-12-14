e17a4fc1b0dbda9838e3ef2d034361a686526e91
kevinMerizalde <kevinmerizalde19@outlook.com>
2022-12-14 18:57:13 +0000

    image widget without funcionality

--- frontend/donations-spa/pages/donation/[id].tsx
+++ frontend/donations-spa/pages/donation/[id].tsx
@@ -2,8 +2,10 @@ import AppMenuBar from ".././AppMenuBar";
 import axios from "axios";
 import React, { useRef, useState , useEffect} from "react";
 import { useRouter } from "next/router";
+import { Galleria } from 'primereact/galleria';
 
-/* export const getStaticPaths = async () =>{
+/* 
+export const getStaticPaths = async () =>{
     const requestOptions = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
@@ -12,33 +14,48 @@ import { useRouter } from "next/router";
     const data = await res.json();
     console.log(data);
 
-    const paths = data.map((donation:any) =>{
+     paths = data.data.map(donation =>{
         return{
 params:{id:donation.id.toString() }
         }
     })
 
-    return {paths, fallback:false}
+    return {paths, fallback:false} 
 }
 
-export const getStaticProps = async(context: any) =>{
-    const id = context.params.id;
+export async function getStaticProps() {
     const requestOptions = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     };
-    const res =  await fetch('http://localhost:8080/donations' + id, requestOptions)
+    const res =  await fetch('http://localhost:8080/donations' + 1, requestOptions)
     const data = await res.json();
     
     return{
         props: {donation: data}
     }
     
-}
- */
+} */
+ 
 function DetailDonation() {
+    const [donation, setDonation] = useState<any>(null);
     const router = useRouter()
     const id = router.query.id
+
+    const responsiveOptions = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
 
     useEffect(() => {
         fetchDonation();
@@ -55,37 +72,28 @@ function DetailDonation() {
             };
              const response = await fetch('http://localhost:8080/donations/'+id, requestOptions)
              const data = await response.json();
+             setDonation(data.message)
              console.log(data);
              
-            /* console.log("Debuggggg");
-            if(!Number.isNaN(Number(router.query.id))){
-                const { data } = await axios({
-                    url: "http://localhost:8080/donation/"+router.query.id,        
-                    method: "GET",
-                    headers: { 'Content-Type': 'application/json' }
-                  });
-            if (data.code != 200) {    
-            } else {
-              console.log("OK");
-              console.log(data);
-            }} */
           } catch (error) {
             if(error instanceof Error){
                  console.log(error);    
         }
     }
-        //const response = await fetch('/api/donations');
-        //const data = await response.json();
-        //console.log(data.data.message.results);
-        //console.log(data.code);
       
         
       }
 
     return (
       <>
-        <AppMenuBar />        
-        <h1>Detail Donation</h1>
+        <AppMenuBar />      
+        <div className="card">
+        <h1>Detail of: {donation.title}</h1>
+        </div>
+        <div>
+        <Galleria value={images} item={itemTemplate} thumbnail={thumbnailTemplate} numVisible={5} responsiveOptions={responsiveOptions}></Galleria>
+        </div>
+        
         <h1>{id}</h1>
       </>
     );
