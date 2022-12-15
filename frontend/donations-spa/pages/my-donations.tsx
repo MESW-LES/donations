464b36118a9062c07bc0e6044aa431f66a040ad3
kevinMerizalde <kevinmerizalde19@outlook.com>
2022-12-15 22:00:54 +0000

    delete a donation

--- frontend/donations-spa/pages/my-donations.tsx
+++ frontend/donations-spa/pages/my-donations.tsx
@@ -1,11 +1,13 @@
 import AppMenuBar from "./AppMenuBar";
-import React, { useState, useEffect, SetStateAction } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { DataView, DataViewLayoutOptions, DataViewLayoutType } from 'primereact/dataview';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
 import { ProductService } from './api/ProductService';
 import { InputText } from 'primereact/inputtext';
 import { useRouter } from "next/router";
+import axios from "axios";
+import { Toast } from "primereact/toast";
 
 function MyDonations() {
 
@@ -19,6 +21,8 @@ const [filteredValue, setFilteredValue] = useState(null);
 const [layout, setLayout] = useState<DataViewLayoutType>('grid');
 // Option that is selected to sort
 const [sortKey, setSortKey] = useState(null);
+//for display messages in the view
+const toast = useRef<any>(null);
 const [sortOrder, setSortOrder] = useState<any>(null);
 const [sortField, setSortField] = useState<any>(null);
 
@@ -31,7 +35,17 @@ const goToPage = (page: string) => {
   router.push(page);
 };
 
-
+const showToast = (
+    severityValue: string,
+    summaryValue: string,
+    detailValue: string
+  ) => {
+    toast.current.show({
+      severity: severityValue,
+      summary: summaryValue,
+      detail: detailValue,
+    });
+  };
 
 //posible options to sort
 const sortOptions = [
@@ -55,6 +69,40 @@ const fetchDonations = async ()=>{
         //console.log(dataViewValue[0].donationImages[0])
     }
     
+  }
+
+  //Fetch delete donation BackEnd
+const deleteDonation = async (id: any)=>{
+    try {
+        const { data } = await axios({
+          url: `http://localhost:8080/donations/${id}`,        
+          method: "DELETE",
+        });
+  
+        if (data.code != 200 && data.code != 201 ) {
+          showToast(
+            "error",
+            "Hey",
+            "oops looks like something went wrong, please try again later."
+          );
+        } else {
+          showToast(
+            "success",
+            "Success Message",
+            "The donation was deleted successfully."
+          );
+          fetchDonations();
+        }
+      } catch (error) {
+        if(error instanceof Error){
+          showToast(
+            "error",
+            "Error " + error.message,
+            error.message
+          );
+        }
+        
+      }
   }
 
  useEffect(() => {
@@ -131,15 +179,16 @@ const dataviewListItem = (data: any) => {
             <div className="flex flex-column md:flex-row align-items-center p-3 w-full">                
                 <img src={`http://localhost:8080/donationsImages/${data.donationImages[0]}.png`} alt={data.title} className="my-4 md:my-0 w-9 md:w-10rem shadow-2 mr-5" />
                 <div className="flex-1 flex flex-column align-items-center text-center md:text-left">
-                    <div className="font-bold text-2xl">{data.name}</div>
+                    <div className="font-bold text-2xl">{data.title}</div>
                     <div className="mb-2">{data.description}</div>            
                     <div className="flex align-items-center">
                         <i className="pi pi-tag mr-2"></i>
-                        <span className="font-semibold">{data.category}</span>
+                        <span className="font-semibold">{data.categories[0].name.toUpperCase()}</span>
                     </div>
                 </div>
                 <div className="flex flex-row md:flex-column justify-content-between w-full md:w-auto align-items-center md:align-items-end mt-5 md:mt-0">
-                    <span className="text-2xl font-semibold mb-2 align-self-center md:align-self-end">{data.publish_date}</span>
+                    <span className="text-2xl font-semibold mb-2 align-self-center md:align-self-end">{data.data.createdDate}</span>
+                    <Button onClick={()=>{deleteDonation(data.id)}} icon="pi pi-trash" label="Delete donation" disabled={data.donationProcess.status.toUpperCase() === 'END' || data.donationProcess.status.toUpperCase() === 'ONGOING' || data.donationProcess.status.toUpperCase() === 'REQUESTED'} className="mb-2 bg-red-300 border-red-400 p-button-sm"></Button>
                     <Button icon="pi pi-pencil" label="Edit donation" disabled={data.donationProcess.status.toUpperCase() === 'END'} className="mb-2 p-button-sm"></Button>
                     <span className={`product-badge status-${data.donationProcess.status.toLowerCase()}`}>{data.donationProcess.status}</span>
                 </div>
@@ -155,7 +204,7 @@ const dataviewGridItem = (data: any) => {
                 <div className="flex flex-wrap gap-2 align-items-center justify-content-between mb-2">
                     <div className="flex align-items-center">
                         <i className="pi pi-tag mr-2" />
-                        <span className="font-semibold">{data.categories[0].name}</span>
+                        <span className="font-semibold">{data.categories[0].name.toUpperCase()}</span>
                     </div>
                     <span className={`product-badge status-${data.donationProcess.status.toLowerCase()}`}>{data.donationProcess.status}</span>
                    
@@ -166,8 +215,11 @@ const dataviewGridItem = (data: any) => {
                     <div className="mb-3">{data.description}</div>                    
                 </div>
                 <div className="flex align-items-center justify-content-between">
-                    <span className="text-2xl font-semibold">{data.publish_date}</span>
+                    <span className="text-2xl font-semibold">{data.createdDate.substring(0,10)}</span>
+                    <div>
+                    <Button onClick={()=>{deleteDonation(data.id)}} icon="pi pi-trash"  disabled={data.donationProcess.status.toUpperCase() === 'END' || data.donationProcess.status.toUpperCase() === 'ONGOING' || data.donationProcess.status.toUpperCase() === 'REQUESTED'} className=" bg-red-300 border-red-400 "></Button>
                     <Button icon="pi pi-pencil" disabled={data.donationProcess.status.toUpperCase() === 'END'} />                
+                    </div>
                 </div>
             </div>
         </div>
@@ -193,6 +245,7 @@ return (
   <h1>Hello, <b>UserName</b></h1>
   <Button className=" flex bg-green-300 border-transparent font-semibold" icon= "pi pi-fw pi-box" label="Add Donation" onClick={() => goToPage("add_donation")}></Button>
   </div>
+  <Toast ref={toast} />
     <div className="grid list-demo">
         <div className="col-12">
             <div className="card">
