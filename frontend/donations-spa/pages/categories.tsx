d02a93da4a6555dabbc9b4e4a30cdbeb2f09639b
kevinMerizalde <kevinmerizalde19@outlook.com>
2022-12-27 14:18:12 -0700

    refresh the categories when create one

--- frontend/donations-spa/pages/categories.tsx
+++ frontend/donations-spa/pages/categories.tsx
@@ -117,6 +117,7 @@ const statusEditor = (options:any) => {
         setName("");
         setCode("");
         setDescription("");
+        fetchCategories();
       };      
     } catch (error) {
       if(error instanceof Error){
